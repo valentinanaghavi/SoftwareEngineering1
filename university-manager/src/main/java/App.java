bbe4f55c9c10861abbe31f3af3930d28047662b2
valentinanaghavi <gissu.valentina.naghavi@uni-weimar.de>
2018-10-29 16:15:23 +0100

    hinzufuegen der Instanzen in der main

--- university-manager/src/main/java/App.java
+++ university-manager/src/main/java/App.java
@@ -2,6 +2,11 @@ public class App {
 
     public static void main(String[] args) 
     {
-        
+        Professor p1 = new Professor("Peter", "Petersson","111");
+        Student p2 = new Student("Findus", "Findling","118354");
+
+        System.out.println(p1.getVorname() + " "+ p1.getNachname() +  ", " + p1.getPersNr());
+        System.out.println(p2.getVorname() + " "+ p2.getNachname() + ", " + p2.getMtrNr());
+
     }
 }
